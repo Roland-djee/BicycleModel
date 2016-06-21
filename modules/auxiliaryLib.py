7c298e3d443421d37ad084aba0c2e51941355ae5
Roland Guichard <roland.guichard@ts.catapult.org.uk>
2016-06-21 09:47:29 +0100

    Simplified library

--- modules/auxiliaryLib.py
+++ modules/auxiliaryLib.py
@@ -8,11 +8,7 @@ def normalisedDesiredDirection(rk, r):
 
 def attractiveForceToDestination(velocity, desiredVelocity, position, target):
     '''Returns the attractive force to a destination'''
-    # define the desired direction
     eAlpha  = normalisedDesiredDirection(target, position)
-    # define the desired velocity
-    vAlpha0 = eAlpha * desiredVelocity
-    # relaxation time [s]
-    tauAlpha = 0.5
-
+    vAlpha0 = eAlpha* desiredVelocity
+    tauAlpha = 1.
     return (vAlpha0 - velocity) / tauAlpha
