6ea679e3d082683b63da59dd2c21431358506602
Roland Guichard <roland.guichard@ts.catapult.org.uk>
2016-06-22 10:30:22 +0100

    Set test for the kinematics.

---
+++ test/testKinematics.py
@@ -0,0 +1,30 @@
+'''
+Created on 22 Jun 2016
+
+@author: RolandGuichard
+'''
+import unittest
+
+from src.bicycle_kinematics import *
+
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testKinematics(self):
+        main()
+        
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testKinematics']
+    unittest.main()
